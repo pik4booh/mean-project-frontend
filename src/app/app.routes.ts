04c5e263ddb5dfde199a12584495574354a9c972
pik4booh <prncrobins2003@gmail.com>
2026-02-28 23:21:44 +0300

    update front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'auth',
+		loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
+	},
+	{
+		path: '',
+		// pathMatch: 'full',
+		loadChildren: () => import('./storefront/storefront.routes').then(m => m.STOREFRONT_ROUTES)
+	},
+
+
+	{ path: '**', redirectTo: '' }
+];
