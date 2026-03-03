8b3ec20f70b569b6c1f37b52f36ce3f432f80a74
Pik4booh <prncrobins2003@gmail.com>
2026-03-03 16:14:05 +0300

    API url corrected

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = { 
   production: true, 
-  apiUrl: 'https://m1p13mean-2217-2231.netlify.app/'  // URL pour la production 
+  apiUrl: 'https://hellomercado.up.railway.app/api/v1/'  // URL pour la production 
 }; 
